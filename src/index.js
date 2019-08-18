34389315a7d65772562a687d4c24a2fcf8f52ff6
eduqg <eduardoqgomes@gmail.com>
2019-08-18 14:25:21 -0300

    Adding express and nodemon

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 const express = require('express');
 
 const app = express();
+
+// Middleware que intercepta requisição na rota raiz
+app.get('/', (req, res) => {
+    return res.send('Hello World');
+});
+
+app.listen(3333);
