16ef3b182599be747d5faac39a92912ccee30777
eduqg <eduardoqgomes@gmail.com>
2019-08-19 20:01:18 -0300

    Resizing image

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,10 +1,14 @@
 const { sequelize } = require('../models');
 
 const Post = sequelize.import('../models/post');
+const sharp = require('sharp');
+const path = require('path');
+// File System
+const fs = require('fs');
 
 module.exports = {
     async index(req, res) {
-        const posts = await Post.findAll({order: sequelize.literal('"createdAt" DESC')});
+        const posts = await Post.findAll({ order: sequelize.literal('"createdAt" DESC') });
         return res.json(posts);
     },
     async store(req, res) {
@@ -13,6 +17,14 @@ module.exports = {
         const { author, place, description, hashtags } = req.body;
         // pego finalename e renomeio para image
         const { filename: image } = req.file;
+
+        await sharp(req.file.path)
+            .resize(500)
+            .jpeg({ quality: 70 })
+            .toFile(path.resolve(req.file.destination, 'resized', image)
+            )
+
+        fs.unlinkSync(req.file.path);
 
         const post = await Post.create({
             author,
