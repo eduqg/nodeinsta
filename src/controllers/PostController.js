d7a9c6124cd2ec2600b9d1e154ef86dd29a5fd97
eduqg <eduardoqgomes@gmail.com>
2019-08-19 18:15:32 -0300

    Multer to upload file

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -5,6 +5,8 @@ module.exports = {
 
     },
     async store(req, res) {
+        console.table(req.body);
+        console.log(req.file);
         return res.json({ok: true});
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const multer = require('multer');
 const PostController = require('./controllers/PostController');
 
 const routes = new express.Router();
+const upload = multer();
 
-routes.post('/posts', PostController.store);
+// image refere ao campo da tabela Posts
+routes.post('/posts', upload.single('image'), PostController.store);
 
 module.exports = routes;
